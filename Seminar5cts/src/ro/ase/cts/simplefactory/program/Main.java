1ddcc534232c6681b525c4b6b13261bdac642f24
Alex Zdrafcu <alexzdrafcu@gmail.com>
2021-03-25 16:18:54 +0200

    simplefactory

---
+++ Seminar5cts/src/ro/ase/cts/simplefactory/program/Main.java
@@ -0,0 +1,23 @@
+package ro.ase.cts.simplefactory.program;
+
+import ro.ase.cts.simplefactory.clase.FactoryPersonal;
+import ro.ase.cts.simplefactory.clase.PersonalSpital;
+import ro.ase.cts.simplefactory.clase.TipPersonal;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		try {
+			FactoryPersonal factory = new FactoryPersonal();
+			PersonalSpital medic = factory.getPersonal(TipPersonal.MEDIC, "Catalin");
+			PersonalSpital asistent = factory.getPersonal(TipPersonal.ASISTENT,"Mihai");
+			System.out.println(medic.toString());
+			System.out.println(asistent.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
