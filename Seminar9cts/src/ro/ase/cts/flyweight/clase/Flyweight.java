8799ac80ef5d2881c8415e741305f3ac3ddfda29
Alex Zdrafcu <alexzdrafcu@gmail.com>
2021-05-14 01:44:10 +0300

    flyweight/strategy

---
+++ Seminar9cts/src/ro/ase/cts/flyweight/clase/Flyweight.java
@@ -0,0 +1,5 @@
+package ro.ase.cts.flyweight.clase;
+
+public interface Flyweight {
+	public void printeazaRezervare(Rezervare rezervare);
+}
---
+++ Seminar9cts/src/ro/ase/cts/strategy/program/Main.java
@@ -0,0 +1,25 @@
+
+package ro.ase.cts.strategy.program;
+
+import ro.ase.cts.strategy.clase.Card;
+import ro.ase.cts.strategy.clase.Client;
+import ro.ase.cts.strategy.clase.Portofel;
+import ro.ase.cts.strategy.clase.Strategy;
+
+public class Main {
+	public static void main(String[] args) {
+		Client client1 = new Client("alex");
+		Client client2 = new Client("zdrafcu");
+		Strategy strategieCash = new Portofel(500);
+		Strategy strategieCard = new Card();
+		client1.setStrategie(strategieCash);
+		client2.setStrategie(strategieCard);
+		
+		client1.platesteNota(300);
+		client2.platesteNota(180);
+		
+		client1.platesteNota(400);
+		client1.setStrategie(strategieCard);
+		client1.platesteNota(400);
+	}
+}
